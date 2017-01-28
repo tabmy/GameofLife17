c737e8fdd3f0af5db16f38ca969911f5ba17f59f
tabmy <tomyabe93@gmail.com>
2017-01-28 17:22:14 +0100

    Added GUI.fxml to project

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/View/GUI.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
