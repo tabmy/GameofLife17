c4d573f65a2313bb7601ac2269fa7aa0e4bf071d
tabmy <tomyabe93@gmail.com>
2017-01-28 17:28:07 +0100

    Added draw method to see functionality with fxml.

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,6 +16,14 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        gc = playArea.getGraphicsContext2D();
     }
+
+
+    @FXML
+    public void draw(){
+        gc.setFill(Color.RED);
+        gc.fillRect(0,0,playArea.getWidth(), playArea.getHeight());
+    }
+
 }
