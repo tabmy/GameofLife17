00b85b5d09ac16ba765814d9d095eea3d21293f6
Tommy Abelsen <tomyabe93@gmail.com>
2017-05-03 16:39:42 +0200

    PatternFormatException javadoc complete.

--- src/Model/PatternFormatException.java
+++ src/Model/PatternFormatException.java
@@ -1,7 +1,17 @@
 package Model;
 
+/**
+ * Thrown when there is an exception regarding the pattern files and pattern formats occuring.
+ * @author Abelsen, Tommy
+ * @author Petrovic, Branislav
+ */
+
 public class PatternFormatException extends Exception {
 
+    /**
+     * Constructs a <code>PatternFormatException</code> with the specified detail message.
+     * @param message
+     */
     public PatternFormatException(String message){
         super(message);
     }
